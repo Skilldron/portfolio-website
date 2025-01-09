48caf30de1de097604c98d2a9ad0662ce280b7b5
Skilldron <harold.delory@gmail.com>
2025-01-09 22:18:53 +0100

    Change icon, add plugin image optimizer

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,18 @@
 import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    ViteImageOptimizer({
+      jpg: {
+        quality: 70,
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
